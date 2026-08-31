3b0f959f3eec10164c370b989609b75fd0175092
Arthur Vieira <arthurssivieira@gmail.com>
2026-08-31 03:08:52 -0300

    feat:Implementa rota POST para atualizacao de inventario

--- server.py
+++ server.py
@@ -1,11 +1,22 @@
 from aiohttp import web
-from database import view_colection
+from database import view_colection, update_inventory
 import json 
+
+async def update_collection(request):
+    dados = await request.json()
+
+    card = dados.get('name')
+    own_qty = dados.get('own_qty', 0)
+    wnt_qty = dados.get('wnt_qty', 0)
+    trd_qty = dados.get('trd_qty', 0)
+
+    update_inventory(card,own_qty,wnt_qty,trd_qty)
+    return web.Response(text="Colecao atualizada com sucesso")
+    
 
 async def get_collection(request):
     filtr = request.query.get('filter', 'all')
     return web.json_response(view_colection(filtr))
-
 
 async def status(request):
     server_data = {
@@ -17,6 +28,7 @@ async def status(request):
 
 app = web.Application()
 app.add_routes([web.get('/api/status', status),
-                web.get('/api/cards', get_collection)])
+                web.get('/api/cards', get_collection),
+                web.post('/api/cards', update_collection)])
 
 web.run_app(app)
